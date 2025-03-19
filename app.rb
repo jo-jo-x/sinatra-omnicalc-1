ac2230aaa3528c259721e198c4d847edf50f42a6
Maurice Ford <mauriceford@gmail.com>
2025-03-19 23:41:19 +0000

    initial commit, erbs created

--- app.rb
+++ app.rb
@@ -2,8 +2,37 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+get("/square/new") do
+  erb(:besquare)
+end
+
+get("/square_root/new") do
+  erb(:berooted)
+end
+
+get("/payment/new") do
+  erb(:apargh)
+end
+
+get("/random/new") do
+  erb(:randomizer)
+end
+
+get("/square/results") do
+  erb(:besquare_results)
+end
+
+get("square_root/results") do
+  erb(:berooted_results)
+end
+
+get("payment/results") do
+  erb(:apargh_results)
+end
+
+get("/random/results") do
+  erb(:randomizer_results)
 end
